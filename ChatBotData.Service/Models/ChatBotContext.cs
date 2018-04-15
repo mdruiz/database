cc1861e560f2bd4f18ac71b2d336091a9a94a2f1
mario ruiz <mario.d.ruiz2@gmail.com>
2018-04-14 22:49:32 -0400

    First Commit

---
+++ ChatBotData.Service/Models/ChatBotContext.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChatBotData.Service.Models
+{
+    public class ChatBotContext : DbContext
+    {
+        public  IConfiguration Configuration { get; set; }
+        //private string Connection;
+
+        public DbSet<User> User { get; set; }
+
+        public ChatBotContext()
+        {
+            //Configuration = configuration;
+            Configuration = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.dev.json").Build();
+
+            //Connection = Configuration.GetSection("ConnectionString").Value;
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder builder)
+        {
+            //builder.UseSqlServer(Connection);
+            builder.UseSqlServer(Configuration.GetSection("ConnectionString").Value);
+
+        }
+
+    }
+}
